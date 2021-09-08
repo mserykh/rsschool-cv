ff920cde9f038a73c0bb7105cbe2073cf91ba593
Marie Serykh <marie.serykh@gmail.com>
2021-09-08 23:24:19 +0200

    refactor: update scroll behaviour

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const scrollToTopButton = document.getElementById("scrollToTop");
-  
-const scrollFunction = () => {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        scrollToTopButton.style.display = "block";
-    } 
-    else {
-        scrollToTopButton.style.display = "none";
-    }
-};
 
-window.addEventListener("scroll", scrollFunction);
+const handleScroll = () => {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopButton.style.display = "block";
+  } 
+  else {
+    scrollToTopButton.style.display = "none";
+  }
+};
 
 const scrollToTop = () => {
-    const distanceFromTop = document.documentElement.scrollTop || document.body.scrollTop;
+  const distanceFromTop = document.documentElement.scrollTop || document.body.scrollTop;
 
-    if (distanceFromTop > 0) {
-        window.requestAnimationFrame(scrollToTop);
-        window.scrollTo(0, distanceFromTop - distanceFromTop / 10);
-    }
+  if (distanceFromTop > 0) {
+    window.scrollTo(0, 0);
+  }
 };
 
-scrollToTopButton.onclick = function(e) {
-    e.preventDefault();
-    scrollToTop();
-}
+document.addEventListener("scroll", handleScroll);
+scrollToTopButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  scrollToTop();
+});
