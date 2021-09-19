45f003794d9780e9290c93aa51a2d48b8956fe41
Marie Serykh <marie.serykh@gmail.com>
2021-09-19 20:32:17 +0200

    feat: implement navigation with a burger icon menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const scrollToTopButton = document.getElementById("scrollToTop");
+const burgerButton = document.querySelector(".page-header__burger");
+const menu = document.querySelector(".page-header-nav");
 
 const handleScroll = () => {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -17,8 +19,15 @@ const scrollToTop = () => {
   }
 };
 
+function toggleMobileMenu() {
+  burgerButton.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+
 document.addEventListener("scroll", handleScroll);
 scrollToTopButton.addEventListener("click", function(e) {
   e.preventDefault();
   scrollToTop();
 });
+
+burgerButton.addEventListener("click", toggleMobileMenu);
