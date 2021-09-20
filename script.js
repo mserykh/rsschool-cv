7cd6d8cc1c205b43aaf27163929075a0e883c897
Marie Serykh <marie.serykh@gmail.com>
2021-09-20 15:56:42 +0200

    feat: animate button on downloading

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const scrollToTopButton = document.getElementById("scrollToTop");
 const burgerButton = document.querySelector(".page-header__burger");
 const menu = document.querySelector(".page-header-nav");
 const siteNav = document.querySelector(".site-navigation");
+const downloadButton = document.querySelector(".btn--download");
+const downloadIcon = downloadButton.querySelector(".download-arrow");
 
 const handleScroll = () => {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -32,6 +34,14 @@ function closeNav() {
   menu.classList.remove("active");
 }
 
+function animateDownloadButton() {
+  downloadIcon.classList.add("animate-download");
+
+  setTimeout(function() {
+    downloadIcon.classList.remove("animate-download");
+  }, 1000);
+}
+
 document.addEventListener("scroll", handleScroll);
 scrollToTopButton.addEventListener("click", function(e) {
   e.preventDefault();
@@ -40,3 +50,4 @@ scrollToTopButton.addEventListener("click", function(e) {
 
 burgerButton.addEventListener("click", toggleMobileNav);
 siteNav.addEventListener("click", closeNav);
+downloadButton.addEventListener("click", animateDownloadButton);
